3c5390fa2583edc945a9fc792079f8d8414d7a44
Rajkumar.D <93990197+1228Rajkumar@users.noreply.github.com>
2023-08-01 12:50:55 +0530

    file added

---
+++ script.js
@@ -0,0 +1,654 @@
+let song_list = [
+  {
+    banner: 'photo/Love-Today.jpg',
+    song_name: 'Pacha Elai',
+    Movie: 'Love-Today',
+    aretist: 'Yuvan Shankar Raja',
+    song: 'music/Pacha-Elai-MassTamilan.dev.mp3',
+    trend: 'yes'
+  },
+  {
+    banner: 'photo/sita-ramam.jpg',
+    song_name: 'Kannukkulle',
+    Movie: 'Sita-Ramam',
+    aretist: 'Vishal Chandrashekhar',
+    song: 'music/Kannukkulle-MassTamilan.dev.mp3',
+    trend: 'yes'
+  },
+  {
+    banner: 'photo/Dippam-Dappam.jpg',
+    song_name: 'Dippam Dappam',
+    Movie: 'Kaathuvaakula Rendu Kaadhal',
+    aretist: 'Anirudh Ravichander',
+    song: 'music/Dippam-Dappam-MassTamilan.so.mp3'
+  },
+  {
+    banner: 'photo/don.jpg',
+    song_name: 'Private Party',
+    Movie: 'Don',
+    aretist: 'Anirudh Ravichander',
+    song: 'music/Private-Party-MassTamilan.dev.mp3',
+    trend: 'yes'
+  },
+  {
+    banner: 'photo/Thiruchitrambalam-Movie-Poster.jpg',
+    song_name: 'Thenmozhi',
+    Movie: 'Thiruchitrambalam',
+    aretist: 'Anirudh Ravichander',
+    song: 'music/Thenmozhi-MassTamilan.dev.mp3'
+  },
+  {
+    banner: 'photo/cobra.jpg',
+    song_name: 'Adheeraa',
+    Movie: 'cobra',
+    aretist: 'A. R. Rahman,',
+    song: 'music/Adheeraa-MassTamilan.dev.mp3'
+  },
+  {
+    banner: 'photo/cobra.jpg',
+    song_name: 'Thumbi Thullal',
+    Movie: 'cobra',
+    aretist: 'A. R. Rahman,',
+    song: 'music/Thumbi-Thullal-MassTamilan.dev.mp3'
+  },
+  {
+    banner: 'photo/cobra.jpg',
+    song_name: 'Uyir Urugudhey',
+    Movie: 'cobra',
+    aretist: 'A. R. Rahman,',
+    song: 'music/Uyir-Urugudhey-MassTamilan.dev.mp3'
+  },
+  {
+    banner: 'photo/cobra.jpg',
+    song_name: 'Tharangini',
+    Movie: 'cobra',
+    aretist: 'A. R. Rahman,',
+    song: 'music/Tharangini-MassTamilan.dev.mp3'
+  },
+  {
+    banner: 'photo/cobra.jpg',
+    song_name: ' Yele Ilanchingamey',
+    Movie: 'cobra',
+    aretist: 'A. R. Rahman,',
+    song: 'music/Yele-Ilanchingamey-MassTamilan.dev.mp3'
+  },
+  {
+    banner: 'photo/vickram.jpg',
+    song_name: 'Pathala Pathala',
+    Movie: 'Vickram',
+    aretist: 'Anirudh Ravichander',
+    song: 'music/Pathala-Pathala-MassTamilan.so.mp3'
+  },
+  {
+    banner: 'photo/vickram.jpg',
+    song_name: 'Vikram (Title Track)',
+    Movie: 'Vickram',
+    aretist: 'Anirudh Ravichander',
+    song: 'music/Vikram-Title-Track-MassTamilan.so.mp3'
+  },
+  {
+    banner: 'photo/vickram.jpg',
+    song_name: 'Wasted',
+    Movie: 'Vickram',
+    aretist: 'Anirudh Ravichander',
+    song: 'music/Wasted-MassTamilan.so.mp3'
+  },
+  {
+    banner: 'photo/vickram.jpg',
+    song_name: 'Porkanda Singam',
+    Movie: 'Vickram',
+    aretist: 'Anirudh Ravichander',
+    song: 'music/Porkanda-Singam-MassTamilan.so.mp3'
+  },
+  {
+    banner: 'photo/vickram.jpg',
+    song_name: 'Once Upon a Time',
+    Movie: 'Vickram',
+    aretist: 'Anirudh Ravichander',
+    song: 'music/Once-Upon-a-Time-MassTamilan.so.mp3'
+  },
+  {
+    banner: 'photo/Kantara.jpg',
+    song_name: 'Singara Siriye',
+    Movie: 'Kantara',
+    aretist: 'B Ajaneesh Loknat',
+    song: 'music/He-Semmandha-Azhage-MassTamilan.dev.mp3'
+  },
+  {
+    banner: 'photo/Kantara.jpg',
+    song_name: 'Rebel Song',
+    Movie: 'Kantara',
+    aretist: 'B Ajaneesh Loknat',
+    song: 'music/Aanamale-Kaatukulla-MassTamilan.dev.mp3'
+  },
+  {
+    banner: 'photo/Kantara.jpg',
+    song_name: 'Karma Song',
+    Movie: 'Kantara',
+    aretist: 'B Ajaneesh Loknat',
+    song: 'music/Manidhanin-Payanathil-MassTamilan.dev.mp3',
+    trend: 'yes'
+  },
+  {
+    banner: 'photo/Kantara.jpg',
+    song_name: 'Varaha Roopam',
+    Movie: 'Kantara',
+    aretist: 'B Ajaneesh Loknat',
+    song: 'music/Varaha-Roopam-Daiva-Va-Rishtam-MassTamilan.dev.mp3',
+    trend: 'yes'
+  },
+  {
+    banner: 'photo/Kantara.jpg',
+    song_name: 'Vaa Poruluy',
+    Movie: 'Kantara',
+    aretist: 'B Ajaneesh Loknat',
+    song: 'music/Vaa-Poruluya-MassTamilan.dev.mp3'
+  },
+  {
+    banner: 'photo/jai-bhim.jpg',
+    song_name: 'Power',
+    Movie: 'Jai Bhim',
+    aretist: 'Sean Roldan',
+    song: 'music/Power-MassTamilan.fm.mp3'
+  },
+  {
+    banner: 'photo/jai-bhim.jpg',
+    song_name: 'Polladha Ulagathiley',
+    Movie: 'Jai Bhim',
+    aretist: 'Sean Roldan',
+    song: 'music/Polladha-Ulagathiley-MassTamilan.fm.mp3'
+  },
+  {
+    banner: 'photo/jai-bhim.jpg',
+    song_name: 'Sendumalli',
+    Movie: 'Jai Bhim',
+    aretist: 'Sean Roldan',
+    song: 'music/Sendumalli-MassTamilan.fm.mp3'
+  },
+  {
+    banner: 'photo/jai-bhim.jpg',
+    song_name: 'Thala-Kodhum',
+    Movie: 'Jai Bhim',
+    aretist: 'Sean Roldan',
+    song: 'music/Thala-Kodhum-MassTamilan.fm.mp3'
+  },
+  {
+    banner: 'photo/jai-bhim.jpg',
+    song_name: 'Vettakaara-Kootam',
+    Movie: 'Jai Bhim',
+    aretist: 'Sean Roldan',
+    song: 'music/Vettakaara-Kootam-MassTamilan.fm.mp3'
+  },
+
+  {
+    banner: 'photo/Adithya-Varma.jpg',
+    song_name: 'Yean Ennai Pirindhaai',
+    Movie: 'Adithya Varma',
+    aretist: 'Sid Sriram',
+    song: 'music/Yean-Ennai-Pirindhaai-MassTamilan.org.mp3'
+  },
+  {
+    banner: 'photo/Ennai-Vittu.jpg',
+    song_name: 'Ennai Vittu',
+    Movie: 'Love-Today',
+    aretist: 'Sid Sriram',
+    song: 'music/Ennai-Vittu-MassTamilan.dev.mp3'
+  },
+  {
+    banner: 'photo/kaadhal en kaviye.jpeg',
+    song_name: 'Kaadhal-En-Kaviye',
+    aretist: 'Sid Sriram',
+    song: 'music/Kaadhal-En-Kaviye-MassTamilan.dev.mp3'
+  },
+  {
+    banner: 'photo/Enai-Noki-Paayum-Thota.jpg',
+    song_name: 'Maru Varthai Pesathey',
+    Movie: 'Enai Noki Paayum Thota',
+    aretist: 'Sid Sriram',
+    song: 'music/Maru-Varthai-Pesathey-MassTamilan.com.mp3'
+  },
+  {
+    banner: 'photo/Oh-My-Kadavule.jpg',
+    song_name: 'Kadhaippoma',
+    Movie: 'Oh My Kadavule',
+    aretist: 'Sid Sriram',
+    song: 'music/Kadhaippoma-MassTamilan.io.mp3'
+  },
+  {
+    banner: 'photo/po urave.jpg',
+    song_name: 'po urave',
+    Movie: 'Kaatrin Mozhi',
+    aretist: 'Sid Sriram',
+    song: 'music/Po-Urave-MassTamilan.com.mp3'
+  },
+  {
+    banner: 'photo/yennai-maatrum-kaadhal.jpg',
+    song_name: 'Yennai Maatrum Kadhale',
+    Movie: 'Naanum Rowdy Dhaan',
+    aretist: 'Sid Sriram',
+    song: 'music/Yennai-Maatrum-Kadhale.mp3'
+  },
+  {
+    banner: 'photo/thalli pogathey.jpg',
+    song_name: 'Thalli pogathey',
+    Movie: 'Achcham Yenbadhu Madamaiyada',
+    aretist: 'Sid Sriram',
+    song: 'music/Thalli-Pogathey.mp3'
+  },
+  {
+    banner: 'photo/sita-ramam.jpg',
+    song_name: 'Kannukkulle',
+    Movie: 'Sita-Ramam',
+    aretist: 'Rajkumar',
+    song: 'music/Original song (mp3cut.net).mp3'
+  },
+  {
+    banner: 'photo/prathip.jpg',
+    song_name: 'Nee-Kavithaigala',
+    Movie: 'Maragadha naanayam',
+    aretist: 'pradeepkumar',
+    song: 'music/Nee-Kavithaigala.mp3',
+    trend: 'yes'
+  },
+
+]
+
+const TREND_LIST = document.querySelector('.Trend-list');
+const SID_LIST = document.querySelector('.sid-list');
+const ANIRUTH_LIST = document.querySelector('.ani-list');
+const Raj_LIST = document.querySelector('.raj-list');
+let trend_div = function(img, song, art, index) {
+  return `<div class="T-song" id="${index}" >
+            <img src="${img}">
+        <h4 class="name-of-song">${song}</h4>
+        <p>${art}</p>
+    </div>`
+}
+// Trending-list
+let trendOutput = "";
+let creatTrendlist = function() {
+
+  song_list.forEach(element => {
+    if (element.trend == 'yes') {
+      trendOutput += trend_div(element.banner, element.song_name, element.aretist, song_list.indexOf(element));
+    }
+  });
+  TREND_LIST.innerHTML = trendOutput;
+}
+
+// Sid Sriram-list
+let sidOutput = "";
+let creatSidlist = function() {
+
+  song_list.forEach(element => {
+    if (element.aretist == 'Sid Sriram') {
+      sidOutput += trend_div(element.banner, element.song_name, element.aretist, song_list.indexOf(element));
+    }
+  });
+  SID_LIST.innerHTML = sidOutput;
+}
+//Aniruth-list
+let aniOutput = "";
+let creatAnilist = function() {
+
+  song_list.forEach(element => {
+    if (element.aretist == 'Anirudh Ravichander') {
+      aniOutput += trend_div(element.banner, element.song_name, element.aretist, song_list.indexOf(element));
+    }
+  });
+  ANIRUTH_LIST.innerHTML = aniOutput;
+}
+//Rajkumar-list
+let rajOutput = "";
+let creatRajlist = function() {
+
+  song_list.forEach(element => {
+    if (element.aretist == 'Rajkumar') {
+      rajOutput += trend_div(element.banner, element.song_name, element.aretist, song_list.indexOf(element));
+    }
+  });
+  Raj_LIST.innerHTML = rajOutput;
+}
+creatTrendlist();
+creatSidlist();
+creatAnilist();
+creatRajlist();
+
+
+
+// song-collections
+const SEARCH_SONG_LIST = document.querySelector('.search-song');
+
+let search_div = function(img, song, art, index) {
+  return `<div class="search-list"  id="${index}" onclick="click_to_play(this.id)">
+        <img src="${img}">
+    <div class="detail">    
+    <h4 class="name-of-song">${song}</h4>
+    <p>${art}</p>
+    </div>
+</div>`
+}
+let collectionOfSongOutput = "";
+let creatCollectionlist = function() {
+
+  song_list.forEach(element => {
+    collectionOfSongOutput += search_div(element.banner, element.song_name, element.aretist, song_list.indexOf(element));
+
+  });
+  SEARCH_SONG_LIST.innerHTML = collectionOfSongOutput;
+}
+
+creatCollectionlist();
+
+
+// search-page
+const HOME_ICON = document.querySelector('#home');
+const SEARCH_ICON = document.querySelector('#search');
+const APP_PAGE = document.querySelector('#search');
+const SEARCH_PAGE = document.querySelector('.search-page');
+
+HOME_ICON.addEventListener('click', function() {
+  SEARCH_PAGE.classList.remove("show-page")
+})
+SEARCH_ICON.addEventListener('click', function() {
+  SEARCH_PAGE.classList.add("show-page")
+})
+
+let searchOutput;
+const SEARCHBOX = document.querySelector('#search-input');
+SEARCHBOX.addEventListener('input', function() {
+  searchOutput = '';
+  if (SEARCHBOX.value == "") {
+    searchOutput = "";
+    SEARCH_SONG_LIST.innerHTML = collectionOfSongOutput;
+  }
+  else {
+    song_list.forEach(element => {
+      if (element.song_name.toLowerCase().includes(SEARCHBOX.value.toLowerCase())) {
+        searchOutput += search_div(element.banner, element.song_name, element.aretist, song_list.indexOf(element));
+      }
+
+    });
+    SEARCH_SONG_LIST.innerHTML = searchOutput;
+  }
+
+});
+
+// Add-icon-color
+let SP_ICONS = document.querySelectorAll('.once');
+SP_ICONS.forEach(element => {
+  element.addEventListener('click', function() {
+    element.classList.toggle('icon-color')
+  });
+});
+
+// click-to-start-song
+const SONG_PLAYING_PAGE = document.querySelector('.song-playing-layout');
+const NOW_PLAYING_SONG = document.createElement('audio');
+const NOW_PLAYING_SONG_PHOTO = document.querySelector('.photo');
+const NOW_PLAYING_SONG_NAME = document.querySelector('.now-playing-song-name');
+const NOW_PLAYING_SONG_ARETIST = document.querySelector('.now-playing-song-aretist');
+
+const FAVORITE_ICON = document.querySelector('.favorite-btn');
+const SONG_RANGE = document.querySelector('.song-range');
+const VOLUME_ICON = document.querySelector('.fa-volume-high');
+const VOLUME_RANGE = document.querySelector('.sound-range');
+const VOLUME_BOX = document.querySelector('.volume');
+const NOW_TIME = document.querySelector('.now-time');
+const TOTAL_TIME = document.querySelector('.total-time');
+
+const PLAY_BTN = document.querySelector('.play-btn');
+const BACK_BTN = document.querySelector('.back-btn');
+const NEXT_BTN = document.querySelector('.forward-btn');
+const SHUFFLE_BTN = document.querySelector('.shuffle-btn');
+const REPEAT_BTN = document.querySelector('.repeat-btn');
+
+
+
+// small-song-playing-window
+const SMALL_SONG_PLAYING_PAGE = document.querySelector('.small-play');
+const SMALL_NOW_PLAYING_SONG = document.createElement('audio');
+const SMALL_NOW_PLAYING_SONG_PHOTO = document.querySelector('.small-photo');
+const SMALL_NOW_PLAYING_SONG_NAME = document.querySelector('.sml-song-name');
+const SMALL_NOW_PLAYING_SONG_ARETIST = document.querySelector('.sml-song-aretist');
+// const SMALL_FAVORITE_ICON =document.querySelector('.small-favorite-btn');
+const SMALL_PLAY_BTN = document.querySelector('.sm-play');
+const SMALL_REPEAT_BTN = document.querySelector('.sm-repeat');
+const SMALL_SONG_RANGE = document.querySelector('.sm-song-range');
+
+
+
+
+let isSong_Playing = false;
+let isSong_Playing_page = false;
+let isRandom_playing = false;
+let isRepeat = false;
+let ref = 0;
+let timer;
+song_load(ref);
+
+function song_load(ref) {
+  clearInterval(timer);
+  time_reset();
+  song_volume();
+  NOW_PLAYING_SONG.src = song_list[ref].song;
+  NOW_PLAYING_SONG.load();
+  NOW_PLAYING_SONG_NAME.innerHTML = `<h2>${song_list[ref].song_name}</h2>`;
+  NOW_PLAYING_SONG_ARETIST.innerHTML = `<p>${song_list[ref].aretist}<p>`;
+  NOW_PLAYING_SONG_PHOTO.innerHTML = `<img src="${song_list[ref].banner}">`;
+  FAVORITE_ICON.innerHTML = `<i class="fa-solid fa-heart" id="${ref}"></i>`;
+
+
+  SMALL_NOW_PLAYING_SONG_NAME.innerHTML = `<h4>${song_list[ref].song_name}</h4>`;
+  SMALL_NOW_PLAYING_SONG_ARETIST.innerHTML = `<p>${song_list[ref].aretist}<p>`;
+  SMALL_NOW_PLAYING_SONG_PHOTO.innerHTML = `<img src="${song_list[ref].banner}">`;
+  // SMALL_FAVORITE_ICON.innerHTML=`<i class="fa-solid fa-heart" id="${ref}"></i>`;
+
+  NOW_PLAYING_SONG.addEventListener('ended', function() {
+    if (isRepeat === true) {
+      repeat_play();
+      play_song();
+    }
+    else {
+      next_song();
+    }
+
+  })
+  timer = setInterval(range_update, 1000);
+
+
+  SMALL_NOW_PLAYING_SONG.addEventListener('ended', next_song);
+
+}
+
+function time_reset() {
+  NOW_TIME.textContent = "00:00";
+  TOTAL_TIME.textContent = "00:00";
+  SONG_RANGE.value = 0;
+}
+
+function repeat_play() {
+  let now_ref = ref;
+  song_load(now_ref);
+  play_song();
+  isRepeat = true;
+  REPEAT_BTN.classList.add('icon-color');
+  SMALL_REPEAT_BTN.classList.add('icon-color');
+}
+
+function stop_repeat_play() {
+  isRepeat = false;
+  REPEAT_BTN.classList.remove('icon-color');
+  SMALL_REPEAT_BTN.classList.remove('icon-color');
+}
+
+function play_song() {
+  NOW_PLAYING_SONG.play();
+  isSong_Playing = true;
+  PLAY_BTN.innerHTML = `<i class="fa-solid fa-circle-pause"></i>`;
+  SMALL_PLAY_BTN.innerHTML = `<i class="fa-solid fa-circle-pause"></i>`;
+}
+
+function pause_song() {
+  NOW_PLAYING_SONG.pause();
+  isSong_Playing = false;
+  PLAY_BTN.innerHTML = `<i class="fa-solid fa-circle-play"></i>`;
+  SMALL_PLAY_BTN.innerHTML = `<i class="fa-solid fa-circle-play"></i>`;
+}
+
+
+function next_song() {
+  if (ref < song_list.length - 1 && isRandom_playing === false) {
+    ref += 1;
+    console.log("dfghj");
+  }
+
+  else if (ref < song_list.length - 1 && isRandom_playing === true) {
+    let randonref = Number.parseInt(Math.random() * song_list.length);
+    ref = randonref;
+    console.log("raja");
+  }
+  else {
+    ref = 0;
+  }
+  song_load(ref);
+  play_song();
+
+}
+
+function goBack_song() {
+  if (ref > 0) {
+    ref -= 1;
+  }
+  else {
+    ref = song_list.length - 1;
+  }
+  song_load(ref);
+  play_song();
+}
+
+
+PLAY_BTN.addEventListener('click', function play_pause_mode() {
+  isSong_Playing ? pause_song() : play_song();
+})
+SMALL_PLAY_BTN.addEventListener('click', function play_pause_mode() {
+  isSong_Playing ? pause_song() : play_song();
+})
+NEXT_BTN.addEventListener('click', next_song);
+BACK_BTN.addEventListener('click', goBack_song);
+SHUFFLE_BTN.addEventListener('click', function() {
+  if (isRandom_playing === true) {
+    isRandom_playing = false;
+  }
+  else {
+    isRandom_playing = true;
+  }
+});
+REPEAT_BTN.addEventListener('click', function replay_Or_Not() {
+  isRepeat ? stop_repeat_play() : repeat_play();
+});
+SMALL_REPEAT_BTN.addEventListener('click', function replay_Or_Not() {
+  isRepeat ? stop_repeat_play() : repeat_play();
+});
+VOLUME_ICON.addEventListener('click', function() {
+  VOLUME_BOX.classList.toggle('show-volume')
+
+});
+MINIMIZE_ICON = document.querySelector('.fa-chevron-down');
+MINIMIZE_ICON.addEventListener('click', function() {
+  SONG_PLAYING_PAGE.classList.add('hide-playing');
+  SMALL_SONG_PLAYING_PAGE.classList.add('show-small-playing-page')
+  isSong_Playing_page = false;
+
+})
+SMALL_NOW_PLAYING_SONG_PHOTO.addEventListener('click', function() {
+  // SONG_PLAYING_PAGE.classList.add('show-playing');
+  SONG_PLAYING_PAGE.classList.remove('hide-playing');
+  SMALL_SONG_PLAYING_PAGE.classList.remove('show-small-playing-page')
+  console.log("dfghj");
+  isSong_Playing_page = true;
+})
+
+
+// Click_To_Play_Main-page-song
+
+
+let SELECT_SONG = document.querySelectorAll('.T-song');
+SELECT_SONG.forEach(element => {
+  element.addEventListener('click', function() {
+    ref = Number(this.id);
+    song_load(ref)
+    play_song();
+    SONG_PLAYING_PAGE.classList.add('show-playing');
+    // SONG_PLAYING_PAGE.classList.remove('hide-playing');
+    isSong_Playing_page = true;
+  })
+});
+// Click_To_Play_Main-page-song
+
+
+let SELECT_SEARCH_SONG = document.querySelectorAll('.search-list');
+function click_to_play(id) {
+  ref = Number(id);
+  song_load(ref)
+  play_song();
+  SONG_PLAYING_PAGE.classList.add('show-playing');
+  isSong_Playing_page = true;
+  // SONG_PLAYING_PAGE.classList.remove('hide-playing');
+};
+
+
+
+// let FAVORITE_BTN = document.querySelectorAll('.fa-heart');
+// FAVORITE_ICON.addEventListener('click',function () {
+//     FAVORITE_BTN.forEach(element => {
+//         element.classList.toggle('icon-color');
+//         console.log(this.id);
+//         console.log(this); 
+//     });
+
+// })
+
+function song_to_time() {
+  let toTime = NOW_PLAYING_SONG.duration * (SONG_RANGE.value / 100);
+  NOW_PLAYING_SONG.currentTime = toTime
+  console.log("j");
+}
+
+function song_volume() {
+  NOW_PLAYING_SONG.volume = VOLUME_RANGE.value / 100;
+  console.log("ef");
+}
+SONG_RANGE.addEventListener('change', song_to_time);
+VOLUME_RANGE.addEventListener('change', song_volume);
+SMALL_SONG_RANGE.addEventListener('change', song_to_time);
+
+function range_update() {
+  let range_position = 0;
+  if (!isNaN(NOW_PLAYING_SONG.duration)) {
+    range_position = NOW_PLAYING_SONG.currentTime * (100 / NOW_PLAYING_SONG.duration);
+    SONG_RANGE.value = range_position;
+    SMALL_SONG_RANGE.value = range_position;
+    let curr_Minute = Math.floor(NOW_PLAYING_SONG.currentTime / 60);
+    let curr_Sec = Math.floor(NOW_PLAYING_SONG.currentTime - curr_Minute * 60);
+    let duration_Minute = Math.floor(NOW_PLAYING_SONG.duration / 60);
+    let duration_Sec = Math.floor(NOW_PLAYING_SONG.duration - duration_Minute * 60);
+    if (curr_Minute < 10) {
+      curr_Minute = "0" + curr_Minute;
+    }
+    if (curr_Sec < 10) {
+      curr_Sec = "0" + curr_Sec;
+    }
+    if (duration_Minute < 10) {
+      duration_Minute = "0" + duration_Minute;
+    }
+    if (duration_Sec < 10) {
+      duration_Sec = "0" + duration_Sec;
+    }
+    NOW_TIME.textContent = curr_Minute + ":" + curr_Sec;
+    TOTAL_TIME.textContent = duration_Minute + ":" + duration_Sec;
+  }
+}
+
+
+
